65cb2e2024ca5e003dbf1eb9fbdc6cf47e2614af
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 15:54:02 +0200

    Add project files.

---
+++ matikka11-1/matikka11_1.py
@@ -0,0 +1,17 @@
+num_test=1
+stop_key=1
+print('test print')
+flag=1
+
+while num_test<1000000:
+    flag = 1
+    num_test=num_test+2
+    
+    for i in range(2,(num_test//2)+1):
+        if(num_test%i == 0):
+            flag = 0
+            break
+        
+    if(flag==1):
+        print('%d is a prime number' %num_test)
+            
